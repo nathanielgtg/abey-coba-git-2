c7e7c097270adff8e7cb16afcccfcfdd30e52581
nathanielgtg <abhey.syalomta@gmail.com>
2021-03-05 15:42:04 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem est, voluptate alias ad velit, error minima nemo ipsum perspiciatis iusto reprehenderit libero rerum maxime modi voluptates facilis voluptatum quaerat dolore!</p> 
+</body>
+</html>
